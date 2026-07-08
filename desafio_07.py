fae03e151663cda31290fdb6c8b5dd37a0250d14
Anderson Luis Da Silva de Deus <andhy.deus.dba@outlook.com>
2026-07-08 15:44:53 -0300

    selecionado itens da lista
    neste desafio, usei o primeiro [0] para selecionar o primeiro item independente de qual for da lista e usei [-1] para selecionar o ultimo item da lista , independente que qual seja mesmo acrescentando mais informações.

---
+++ desafio_07.py
@@ -0,0 +1,5 @@
+frutas = ['Maça', 'banana', 'manga', 'uva', 'goiaba', 'laranja', 'abacaxi', 'melancia', 'morango', 'kiwi', 'pera']
+
+
+print(f'A primeira fruta é: {frutas[0]}')
+print(f'A ultima fruta é: {frutas[-1]}')
